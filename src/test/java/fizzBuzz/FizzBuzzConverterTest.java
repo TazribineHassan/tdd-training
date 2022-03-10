9e3525657125b7de030dd9c1895287f674f05d82
TazribineHassan <tazribinehassan1998@gmail.com>
2022-03-10 17:55:31 +0100

    shouldReturnBuzzWhenInputIsMultipleOf5

--- src/test/java/fizzBuzz/FizzBuzzConverterTest.java
+++ src/test/java/fizzBuzz/FizzBuzzConverterTest.java
@@ -2,16 +2,23 @@ package fizzBuzz;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FizzBuzzConverterTest {
 
 
     @Test
-    public void shouldReturnFizzWhenInputIsMultipleOf3(){
-        FizzBuzzConverter fizzBuzzConverter=new FizzBuzzConverter();
-        String result=fizzBuzzConverter.convert(3);
-        assertEquals("Fizz",result);
+    public void shouldReturnFizzWhenInputIsMultipleOf3() {
+        FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
+        String result = fizzBuzzConverter.convert(3);
+        assertEquals("Fizz", result);
+    }
+
+    @Test
+    public void shouldReturnBuzzWhenInputIsMultipleOf5() {
+        FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
+        String result = fizzBuzzConverter.convert(10);
+        assertEquals("Buzz", result);
     }
 
 }
