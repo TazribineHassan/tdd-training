ec15056ecb54a9a64ae8393c4c12f179007b7650
TazribineHassan <tazribinehassan1998@gmail.com>
2022-03-10 19:07:27 +0100

    shouldReturnListFrom1ToInputWhenInputIsGiven

--- src/main/java/fizzBuzz/ListFactory.java
+++ src/main/java/fizzBuzz/ListFactory.java
@@ -1,10 +1,13 @@
 package fizzBuzz;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ListFactory {
     public List<Integer> generateList(int input) {
+
         if (input == 0) throw new RuntimeException("Number must be greater then 0");
-        return List.of(1, 2, 3, 4);
+        return IntStream.rangeClosed(1, input).boxed().collect(Collectors.toList());
     }
 }
--- src/test/java/fizzBuzz/ListFactoryTest.java
+++ src/test/java/fizzBuzz/ListFactoryTest.java
@@ -25,4 +25,14 @@ public class ListFactoryTest {
         RuntimeException exception = assertThrows(RuntimeException.class, () -> factory.generateList(0));
         assertEquals("Number must be greater then 0", exception.getMessage());
     }
+
+    @Test
+
+    public void shouldReturnListFrom1ToInputWhenInputIsGiven() {
+
+        int input = 7;
+        ListFactory factory = new ListFactory();
+        List<Integer> result = factory.generateList(input);
+        assertArrayEquals(List.of(1, 2, 3, 4, 5, 6, 7).toArray(), result.toArray());
+    }
 }
