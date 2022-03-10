785fe6c1390b32fb5c212e381d93dc4024f1bf4c
TazribineHassan <tazribinehassan1998@gmail.com>
2022-03-10 18:55:19 +0100

    'shouldThrowRunTimeExceptionWhenInputIs0'

--- src/main/java/fizzBuzz/ListFactory.java
+++ src/main/java/fizzBuzz/ListFactory.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class ListFactory {
     public List<Integer> generateList(int input) {
+        if (input == 0) throw new RuntimeException("Number must be greater then 0");
         return List.of(1, 2, 3, 4);
     }
 }
--- src/test/java/fizzBuzz/ListFactoryTest.java
+++ src/test/java/fizzBuzz/ListFactoryTest.java
@@ -1,11 +1,10 @@
 package fizzBuzz;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ListFactoryTest {
 
@@ -16,6 +15,14 @@ public class ListFactoryTest {
 
         List<Integer> result = factory.generateList(input);
 
-        assertArrayEquals(List.of(1,2,3,4).toArray(), result.toArray());
+        assertArrayEquals(List.of(1, 2, 3, 4).toArray(), result.toArray());
+    }
+
+    @Test
+    public void shouldThrowRunTimeExceptionWhenInputIs0() {
+        int input = 4;
+        ListFactory factory = new ListFactory();
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> factory.generateList(0));
+        assertEquals("Number must be greater then 0", exception.getMessage());
     }
 }
